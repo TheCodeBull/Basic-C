74f8a1d5b5a49fb04ff8bceb8f8ff248bdba4565
TheHarshal-WASD <161223153+TheHarshal-WASD@users.noreply.github.com>
2024-06-23 16:46:57 +0530

    Add files via upload

---
+++ harshu1a.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("Hello Pale Blue Dot");
+    return 0;
+}
---
+++ harshu1b.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+   printf("Hello India\n");
+   printf("Maharashtra Mazha");
+   return 0; 
+}
