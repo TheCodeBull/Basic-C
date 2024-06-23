4060f819d2e92268178a66595692fa4d0d974475
TheHarshal-WASD <161223153+TheHarshal-WASD@users.noreply.github.com>
2024-06-23 16:29:54 +0530

    Add files via upload

---
+++ harshu2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    char name[]="Harshal";
+    char NAME[]="Mayur";
+    int age=20;
+    int AGE=24;
+    printf("My name is %s\n",name);
+    printf("my broski is %s\n",NAME);
+    printf("My age is %d\n",age);
+    printf("his age is %d\n",AGE);
+}
