74d5ac90d7ae816238773572d5bad7d69c961b57
Harshal Ingole <161223153+TheCodeBull@users.noreply.github.com>
2024-06-24 13:14:48 +0530

    Update harshu1b.c

--- harshu1b.c
+++ harshu1b.c
@@ -3,5 +3,8 @@ int main()
 {
    printf("Hello India\n");
    printf("Maharashtra Mazha");
+   /*main() is main method ;
+     \n is for next line ;
+     return 0 is for successful exectution of code vis-a-vis C.; */
    return 0; 
 }
