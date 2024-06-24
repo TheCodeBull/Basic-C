85f16d4d805dd1e724df830a88e947cb941dc6be
Harshal Ingole <161223153+TheCodeBull@users.noreply.github.com>
2024-06-24 13:09:58 +0530

    Add files via upload

---
+++ mathfunction.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+/*to access mathFunction library <math.h> is needed.*/
+/*this are just some basic maths function .*/
+int main() 
+{
+  /*%d for Integer no.
+    %f for Decimal no.*/ 
+    printf("%d\n", 23+79 );
+    printf("%f\n", 23.5+78 );
+    printf("%d\n", 200/25);
+    printf("%f\n", 13.5/6 );
+    printf("%f\n", pow(2,3));
+    printf("%f\n", sqrt(49) );
+    printf("%f\n",sin(0.52359878));
+    printf("%f\n",cos(0.52359878));
+    /*30degree =0.52359878 radian */
+    return 0;
+}    
